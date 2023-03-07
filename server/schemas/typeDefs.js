2237e406fbe297090e17e1c4465a6dfbaa9f404b
dorecio <dorisrecio@gmail.com>
2023-03-06 22:04:15 -0600

    se modifica archivo TypeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,61 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  scalar MyDate
+ 
+  type User {
+    _id:ID
+    username:String
+    email:String
+    password: String
+    rol:String!
+    themes:[Theme]!
+    fechas:[Schedule]!
+  }
 
   type Schedule {
     _id: ID!
-    scheduleDate:MyDate
+    scheduleDate:String
     scheduleHour:Int 
     teacher:Int
-    busy:Boolean
+    theme:Int
+    student:Int
     }
 
+  type Theme {
+    _id:ID!
+    name:String
+    teachers:[User]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    users:[User]!
+    user(username: String!):User
+    
+    teachers:[User]!
+    students:[User]!
+        
     schedules:[Schedule]
-    schedule(scheduleId: ID!): Schedule
+    schedule(_id: ID!): Schedule
+
+    themes:[Theme]
+    theme(_id:ID!):Theme
+    
+    me: User
   }
 
   type Mutation {
-    addSchedule(schedule:Schedule ):Schedule
-    removeSchedule(id:ID!):Schedule
-    updateSchedule(scheduleDate:MyDate, scheduleHour:Int,teacher:Int):Schedule
+    addUser(username: String!, email: String!, password: String!,rol:String!): Auth
+    login(email: String!, password: String!): Auth
+    removeUser: User
+    addSchedule(scheduleDate:String!, scheduleHour:Int!, theme:String! ):Schedule
+    updateSchedule(_id:ID!, scheduleDate:String, scheduleHour:Int,teacher:Int):Schedule
+    removeSchedule(_id:ID!):Schedule
+    #addTheme:
   }
 `;
 
