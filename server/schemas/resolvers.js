5a10e8f41064354daf8d577f00dcfcc8d5447f96
dorecio <dorisrecio@gmail.com>
2023-03-06 22:12:27 -0600

    se modifica archivo resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,74 +1,124 @@
-const { Schedule } = require('../models');
+const { Schedule, User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-import { GraphQLScalarType } from 'graphql';
-import { Kind } from 'graphql/language';
-import dayjs from "dayjs";
-
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        users: async () => {
+            return User.find().populate('fechas');;
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ userName: username }).populate('fechas', 'temas');
+        },
+
+        teachers: async () => {
+            return User.findAll(User.rol === "maestro").populate('fechas', 'temas');
+        },
+
+        students: async () => {
+            return User.findAll(User.rol === "estudiente").populate('fechas', 'temas');
+        },
+
         schedules: async () => {
             return Schedule.findAll();
         },
         schedule: async (parent, { scheduleId }) => {
             return Schedule.findOne({ _id: scheduleId });
         },
-    },
-    MyDate: new GraphQLScalarType({
-            name: 'Date',
-            description: 'Date custom scalar type',
-            parseValue(value) {
-                return dayjs(value); // value from the client
-            },
-            serialize(value) {
-                return dayjs(value).format("DD-MM-YYYY"); // value sent to the client
-            },
-            parseLiteral(ast) {
-                if (ast.kind === Kind.STRING) {
-                    return dayjs(ast.value) // ast value is always in string format
-                }
-                return null;
+
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
             }
-        }),
-    
+            throw new AuthenticationError('Necesitas estar registrado!');
+        },
+    },
+
+
     Mutation: {
-        addSchedule: async (parent, args) => {
-            if (Schedule.find(s => (s.scheduleDate === args.scheduleDate && s.scheduleHour === args.scheduleHour))){
-                throw new AuthenticationError('Este horario ya está creado!');
+        addUser: async (parent, { username, email, password, rol }) => {
+            // Crear usuario
+            const user = await User.create({ username, email, password, rol });
+            // Firmamar un token web de JSON e iniciar sesión después de crearlo
+            const token = signToken(user);
+            // Devolver un objeto `Auth` que consta del token firmado y la información del usuario
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            // Buscar al usuario por la dirección de correo electrónico proporcionada
+            const user = await User.findOne({ email });
+
+            //Si no existe el usuario, se envía error de autenticación 
+            if (!user) {
+                throw new AuthenticationError('No existe usuario con esta dirección de correo electrónico');
             }
 
-            const schedule = await Schedule.create({
-                scheduleDate: args.scheduleDate,
-                scheduleHour: args.scheduleHour,
-                teacher: args.teacher,
-                busy: 0
-            });
-            await Schedule.Update(
-                { $addToSet: { schedules: schedule } }
-            );
-            
-            return schedule;
-        }
-    },
+            // Si existe el usuario se ejecuta el método de instancia `isCorrectPassword` y verificamos si la contraseña es correcta
+            const correctPw = await user.isCorrectPassword(password);
 
-        removeSchedule: async (parent, { scheduleId }) => {
-            return Schedule.findOneAndDelete({ _id: scheduleId});
+            // Si la contraseña es incorrecta, se devuelve error de autenticación 
+            if (!correctPw) {
+                throw new AuthenticationError('Contraseña incorrecta');
+            }
+
+            // Si el correo electrónico y contraseña son correctos, se inicia sesión en la app con un JWT
+            const token = signToken(user);
+
+            // Devolver un objeto `Auth` que consta del token firmado y la información del usuario
+            return { token, user };
+        },
+        removeUser: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOneAndDelete({ _id: context.user._id });
+            }
+            throw new AuthenticationError('Necesitas estar registrado!');
         },
 
-        updateSchedule: async (parent, { scheduleId, newDate, newHour,newTeacher }) => {
-           return await Schedule.findOneAndUpdate(
-                { _id: scheduleId },
-               {
-                   $pull: {
-                       scheduleDate: newDate,
-                       scheduleHour: newHour,
-                       teacher:newTeacher
-                   }
-               },
-                //Devuelvo objeto actualizado
-                { new: true }
-            );
-        }
-    }
+        addSchedule: async (parent, args, context) => {
+            if (context.user) {
+                const data = Schedule.find(s => (s.scheduleDate === args.scheduleDate && s.scheduleHour === args.scheduleHour))
+                console.log('ESTE ES EL DATO', data);
+                if (data) {
+                    throw new AuthenticationError('Este horario ya está creado!');
+                }
+                const schedule = await Schedule.create(args);
+
+                await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { fechas: schedule._id } }
+                );
+
+                return schedule;
+            } else {
+                throw new AuthenticationError('Necesitas estar registrado!');
+            }
+        },
+
+        updateSchedule: async (parent, { scheduleId, newDate, newHour, newTeacher }) => {
+            if (context.user) {
+                return await Schedule.findOneAndUpdate(
+                    { _id: scheduleId },
+                    {
+                        $pull: {
+                            scheduleDate: newDate,
+                            scheduleHour: newHour,
+                            teacher: newTeacher
+                        }
+                    },
+                    //Devuelvo objeto actualizado
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('Necesitas estar registrado!');
+        },
+
+        removeSchedule: async (parent, { scheduleId }, context) => {
+            if (context.user) {
+                return Schedule.findOneAndDelete({ _id: scheduleId });
+            }
+            throw new AuthenticationError('Necesitas estar registrado!');
+        },
+    },
+};
 
 module.exports = resolvers;
